103476546064b9f2c47d5f494e3219d0098d5987
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2021-07-27 13:11:18 +0200

    Calculate exon size

--- splice/parse_gtf.py
+++ splice/parse_gtf.py
@@ -4,18 +4,43 @@ import argparse
 import json
 
 
+def exon_size(exon):
+    """ Calculate the size of an exon
+
+    The begin of an attribute in GTF is 1-based, and inclusive
+    The end of an attribute in GTF is also inclusive.
+
+    See https://www.ensembl.org/info/website/upload/gff.html
+
+    For example, setting start-end to 1-2 describes two bases, the first and second base in the sequence
+    """
+    start = int(exon['start'])
+    end = int(exon['end'])
+    return end - start + 1
+
 def main(args):
+    exons = list()
     for record in parse_gtf(args.gtf):
-        print(json.dumps(record, indent=True))
-        exit()
+        if record['feature'] == 'exon':
+            exons.append(record)
+
+    old_name = None
+    for exon in exons:
+        attr = exon['attribute']
+        name = attr.get('transcript_name')
+        if name != old_name:
+            print('-'*20)
+            old_name = name
+        number = attr.get('exon_number')
+        size = exon_size(exon)
+        assert size > 0
+        print(f'{name}:{number}\t{size}\t{size%3}')
+    #print(json.dumps(exons, indent=True))
 
 def parse_attribute(attribute):
     """ attribute - A semicolon-separated list of tag-value pairs """
     attributes = dict()
     for attr in attribute.split(';')[:-1]:
-        #print(attr)
-        #print(attr.strip().split(' '))
-        #print('=='*40)
         key, *value = attr.strip().split(' ')
         attributes[key] = ' '.join(val.replace('"','') for val in value)
     return attributes
